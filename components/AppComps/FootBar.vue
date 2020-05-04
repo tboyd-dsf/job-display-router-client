64394f8f7861e673bbd581cfeb542e07ad974c03
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-04 18:04:00 -0500

    app bars (header and footer) added and styled

---
+++ components/AppComps/FootBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card>
+    <v-footer>
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="red darken-4 text-center"
+      >
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Design Source Flooring, LLC</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/AppComps/HeadBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app-bar
+        absolute
+        app
+        dark
+        color="red darken-4"
+        flat>
+        <v-toolbar-title>JayLoc Router</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
