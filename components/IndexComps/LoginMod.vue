1ef4d8e16d829c4170e4bcfb4a4dec13da734a88
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-07 16:14:35 -0500

    logging in posts to server, logs in successfully

--- components/IndexComps/LoginMod.vue
+++ components/IndexComps/LoginMod.vue
@@ -5,12 +5,13 @@
       <v-card-text>
         <v-row>
           <v-col cols="12">
-            <v-text-field label="Email" type="email" clearable></v-text-field>
+            <v-text-field v-model="email" label="Email" type="email" clearable></v-text-field>
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12">
             <v-text-field
+              v-model="pass"
               label="Password"
               type="password"
               clearable
@@ -19,14 +20,36 @@
         </v-row>
       </v-card-text>
       <v-card-actions>
-        <v-btn class="red darken-4 mt-2">Log In</v-btn>
+        <v-btn class="red darken-4 mt-2" @click="loginUser">Log In</v-btn>
       </v-card-actions>
     </v-form>
   </v-card>
 </template>
 
 <script>
-export default {}
+import axios from 'axios';
+export default {
+  data: () => {
+    return {
+      email: '',
+      pass: ''
+    }
+  },
+  methods: {
+    loginUser() {
+      console.log('loginUser function running')
+      axios.post("http://localhost:3030/api/login", {
+        email: this.email,
+        password: this.pass
+      })
+      .then(res => {
+        console.log(res)
+        this.email = ''
+        this.pass = ''
+      })
+    }
+  }
+}
 </script>
 
 <style scoped></style>
