a213f98a6673fbbdc30e769c545d149ce05988f7
Tyler Boyd <tylerb@theboydprojects.com>
2020-05-29 09:12:45 -0500

    removed plugin that wasn't working

--- plugins/GoogleMaps.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
-
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyBixdZ59lvciQCEdgRo3ggV2-UyDZeK9jA',
-        libraries: 'places'
-    }
-})
